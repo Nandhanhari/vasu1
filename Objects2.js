ff2b214e95d0b2024f12fcc20b08bd6277565c68
Nandhanhari <108936010+Nandhanhari@users.noreply.github.com>
2022-07-12 14:37:08 +0530

    changes made

---
+++ Objects2.js
@@ -0,0 +1,16 @@
+let Studdetails = [
+    {name:"vasu", age:27, gender:"m"},    {name:"vasu", age:27, gender:"m"}, 456
+
+]
+console.log(Studdetails)
+console.log(Studdetails.length)
+console.log(Studdetails[1])
+let marks = [
+    [23, 33, 43],
+    [33, 34, 54]
+]
+let marks2 = [
+    [44, 46, 49],
+    [36, 39, 38]
+]
+console.log(marks + marks2)
