64a8ec4daac465d0f3daf6857b17c3fd9d225eb8
Nandhanhari <108936010+Nandhanhari@users.noreply.github.com>
2022-07-13 16:59:08 +0530

    Loops concept

---
+++ WebProject/Whilefor.js
@@ -0,0 +1,54 @@
+let i = 1
+while (i <= 10){
+    console.log(i)
+    i++
+}
+/* Print numbers from 10 to 1 */
+let j = 10
+while (j >= 1) {
+    console.log(j)
+    j--
+}
+/* program to print 5 to 15 numbers*/
+for(k =10; k <= 15; k++) {
+    console.log(k)
+}
+/* program to print 15 to 5 numbers*/
+for(l = 15; l >= 10; l--) {
+    console.log(l)
+}
+/* Program to rpint even numbers */
+for (let m =1; m <= 15; m++) {
+    if (m % 2 == 0) {
+        console.log(" This number are even number" + m)
+    }
+    else if (m % 2 == !0){
+        console.log ("This number are odd number" + m )
+    }
+    else {
+    console.log("No numbers are available")
+    }
+}
+/* Program to pint 4 multiples numbers */
+/*let n =1 
+while(n <= 10) {
+    console.log("4"  * n)
+    n++
+}
+ Program to print 6 multiples numbers */
+let n =1 
+while(n <= 10) {
+    console.log("6" * n)
+    n++
+}
+/* Program to print 24 factors numbers */
+let p = 1
+while (p <= 24) {
+    if (24 % p == 0) {
+        console.log(p + " number is factor of 24")
+    }
+    p++
+}
+
+
+
